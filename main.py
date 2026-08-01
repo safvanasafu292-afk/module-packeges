a4e2cb3e829e01b42c12d8e11e629c54bb2cef95
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-01 18:06:40 +0530

    --init--,calculator,greet

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import utility
+from my_packages import calculator,greet
 print(utility.greet("rays"))
 print(utility.add(5,10))
+
+print(greet.say_hello("safu"))
+print()
+add=calculator.add(100,200)
+print("add:",add)
+sub=calculator.subtract(add,5)
+print("sub:",sub)
