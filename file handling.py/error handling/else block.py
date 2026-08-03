1fa4fd0381d87e688ddd66330a458207c2687b82
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-03 20:00:05 +0530

    finnaly error block

---
+++ file handling.py/error handling/else block.py
@@ -0,0 +1,10 @@
+try:
+    number=int (input("enter a number:"))
+    result=10/number
+    print("result",result)
+except ValueError:
+    print("please enter a valid number")
+except ZeroDivisionError:
+    print("cannot divided by zero")
+else:
+    print("calculation successfull")
---
+++ file handling.py/error handling/finally error block.py
@@ -0,0 +1,12 @@
+try:
+    number=int (input("enter a number:"))
+    result=10/number
+    print("result",result)
+except ValueError:
+    print("please enter a valid number")
+except ZeroDivisionError:
+    print("cannot divided by zero")
+else:
+    print("calculation sucessfully")
+finally:
+    print("prg ended.Thankyou for using")
