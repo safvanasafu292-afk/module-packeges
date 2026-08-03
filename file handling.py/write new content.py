4991a7e8e4d2b1f6f251cd18691889ab83307b40
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-03 12:56:24 +0530

    file handling append

--- file handling.py/write new content.py
+++ file handling.py/write new content.py
@@ -1,7 +1,4 @@
-file=open("notes.txt","w")
-file.write("hello! this is my first note")
-file.close()
 
-file = open("notes.txt", "w")
-file.write("Hello")
+file = open("notes.txt", "a")
+file.write("\nHello ,second line" )
 file.close()
