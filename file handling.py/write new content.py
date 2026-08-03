c5ed36f7392f9fc04e12c3eaf97241322ad27e3c
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-03 12:43:14 +0530

    file handling

---
+++ file handling.py/write new content.py
@@ -0,0 +1,7 @@
+file=open("notes.txt","w")
+file.write("hello! this is my first note")
+file.close()
+
+file = open("notes.txt", "w")
+file.write("Hello")
+file.close()
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import utility
-from my_packages import calculator,greet
-print(utility.greet("rays"))
-print(utility.add(5,10))
+# import utility
+# from my_packages import calculator,greet
+# print(utility.greet("rays"))
+# print(utility.add(5,10))
 
-print(greet.say_hello("safu"))
-print()
-add=calculator.add(100,200)
-print("add:",add)
-sub=calculator.subtract(add,5)
-print("sub:",sub)
+# print(greet.say_hello("safu"))
+# print()
+# add=calculator.add(100,200)
+# print("add:",add)
+# sub=calculator.subtract(add,5)
+# print("sub:",sub)
+
+
