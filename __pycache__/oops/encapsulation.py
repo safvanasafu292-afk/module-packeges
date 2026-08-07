3f8b28c14657cebf0f2c57e8fc88660d2a048954
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-07 16:15:46 +0530

    encapsulation

---
+++ __pycache__/oops/encapsulation.py
@@ -0,0 +1,62 @@
+# class Demo:
+
+#     def __init__(self):
+#         self.public_var = "I AM PUBLIC"
+#         self._protected_var = "I AM PROTECTED"
+#         self.__private_var = "I AM PRIVATE"
+
+#     def show(self):
+#         print(self.public_var)
+#         print(self._protected_var)
+#         print(self.__private_var)
+#     def get_private(self):
+#         return self.__private_var
+    
+# obj = Demo()
+# print(obj.public_var)
+# print(obj._protected_var)
+# print(obj.get_private())
+# #student program
+# class Student:
+
+#     def __init__(self, name, mark):
+#         self.name = name          # Public
+#         self._grade = "A"         # Protected
+#         self.__mark = mark        # Private
+
+#     def get_mark(self):
+#         return self.__mark
+
+
+# student = Student("Safvana", 90)
+
+# print(student.name)
+# print(student._grade)
+# print(student.get_mark())
+#employee
+class Employee:
+    def __init__(self):
+        self.name = "Anu"
+        self._department = "HR"
+        self.__salary = 50000
+
+    def get_salary(self):
+        return self.__salary
+
+emp = Employee()
+
+print(emp.name)
+print(emp._department)
+print(emp.get_salary())
+#bank balance
+class bankaccount:
+    def __init__(self):
+        self.owner="rahul"
+        self._branch="kpm"
+        self.__balance=90000
+    def get_balance(self):
+        return self.__balance
+act=bankaccount()
+print(act.owner)
+print(act._branch)
+print(act.get_balance())        
--- __pycache__/oops/practical for opps.py
+++ __pycache__/oops/practical for opps.py
@@ -1,98 +1,98 @@
-# #I Ask the user for a number and print its square.• Handle ValueError if the user types text instead of a number.
-# try:
-#     num = int(input("Enter a number: "))
-#     print("Square:", num ** 2)
-# except ValueError:
-#     print("Please enter a valid number.")
-# #2 Create a program that divides two numbers entered by the user.Handle both ZeroDivisionError and ValueError.
-# try:
-#     num1 = int(input("Enter first number: "))
-#     num2 = int(input("Enter second number: "))
-#     print("Result:", num1 / num2)
-# except ZeroDivisionError:
-#     print("Cannot divide by zero.")
-# except ValueError:
-#     print("Please enter valid numbers.")
+#I Ask the user for a number and print its square.• Handle ValueError if the user types text instead of a number.
+try:
+    num = int(input("Enter a number: "))
+    print("Square:", num ** 2)
+except ValueError:
+    print("Please enter a valid number.")
+#2 Create a program that divides two numbers entered by the user.Handle both ZeroDivisionError and ValueError.
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Result:", num1 / num2)
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+except ValueError:
+    print("Please enter valid numbers.")
 
-# #Write a program that opens a file named "data.txt" and prints its contents. If the file does't exist, show "File not found!".
-# try:
-#     with open("data.txt", "r") as file:
-#         print(file.read())
-# except FileNotFoundError:
-#     print("File not found!")
-# #4 Ask the user to enter their age.If they enter a negative number, raise a ValueError with message "Age cannot be negative!".
-# try:
-#     age = int(input("Enter your age: "))
+#Write a program that opens a file named "data.txt" and prints its contents. If the file does't exist, show "File not found!".
+try:
+    with open("data.txt", "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("File not found!")
+#4 Ask the user to enter their age.If they enter a negative number, raise a ValueError with message "Age cannot be negative!".
+try:
+    age = int(input("Enter your age: "))
 
-#     if age < 0:
-#         raise ValueError("Age cannot be negative!")
+    if age < 0:
+        raise ValueError("Age cannot be negative!")
 
-#     print("Your age is", age)
+    print("Your age is", age)
 
-# except ValueError as age:
-#     print(age)
-# #Create a calculator that supports +, - *, /Use try-except to handle invalid operators or division by zero. Cl
-# try:
-#     num1 = float(input("Enter first number: "))
-#     operator = input("Enter operator (+,-,*,/): ")
-#     num2 = float(input("Enter second number: "))
+except ValueError as age:
+    print(age)
+#Create a calculator that supports +, - *, /Use try-except to handle invalid operators or division by zero. Cl
+try:
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+,-,*,/): ")
+    num2 = float(input("Enter second number: "))
 
-#     if operator == "+":
-#         print("Result:", num1 + num2)
+    if operator == "+":
+        print("Result:", num1 + num2)
 
-#     elif operator == "-":
-#         print("Result:", num1 - num2)
+    elif operator == "-":
+        print("Result:", num1 - num2)
 
-#     elif operator == "*":
-#         print("Result:", num1 * num2)
+    elif operator == "*":
+        print("Result:", num1 * num2)
 
-#     elif operator == "/":
-#         print("Result:", num1 / num2)
+    elif operator == "/":
+        print("Result:", num1 / num2)
 
-#     else:
-#         raise ValueError("Invalid operator!")
+    else:
+        raise ValueError("Invalid operator!")
 
-# except ZeroDivisionError:
-#     print("Cannot divide by zero.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
 
-# except ValueError as e:
-#     print(e)
-# #6 Use a try-except-else block:If no error oçcurs, print "No errors! Everything went fine."
-# try:
-#     num = int(input("Enter a number: "))
-# except ValueError:
-#     print("Invalid input.")
-# else:
-#     print("No errors! Everything went fine.")
-# #Add a finalif bfock to any of the above programs to print "Program execution complete." every time
-# try:
-#     num = int(input("Enter a number: "))
-#     print("Number:", num)
-# except ValueError:
-#     print("Invalid input.")
-# finally:
-#     print("Program execution complete.")
-# #Write a small program to convert a string to an integer.
-# try:
-#     text = input("Enter a number: ")
-#     number = int(text)
-#     print("Integer:", number)
-# except ValueError:
-#     print("Conversion failed!")
+except ValueError as e:
+    print(e)
+#6 Use a try-except-else block:If no error oçcurs, print "No errors! Everything went fine."
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input.")
+else:
+    print("No errors! Everything went fine.")
+#Add a finalif bfock to any of the above programs to print "Program execution complete." every time
+try:
+    num = int(input("Enter a number: "))
+    print("Number:", num)
+except ValueError:
+    print("Invalid input.")
+finally:
+    print("Program execution complete.")
+#Write a small program to convert a string to an integer.
+try:
+    text = input("Enter a number: ")
+    number = int(text)
+    print("Integer:", number)
+except ValueError:
+  #  print("Conversion failed!")
 
- #Ask the user to enter a file name. Try opening and reading it.
-# try:
-#     filename = input("Enter file name: ")
+ #Ask the user to enter a file name. Try opening and reading it.Use finally to print "File operation finished." even if an error occurs.
 
-#     with open(filename, "r") as file:
-#         print(file.read())
+    filename = input("Enter file name: ")
 
-# except FileNotFoundError:
-#     print("File not found!")
+    with open(filename, "r") as file:
+        print(file.read())
 
-# finally:
-#     print("File operation finished.")
-#
+except FileNotFoundError:
+    print("File not found!")
+
+finally:
+    print("File operation finished.")
+#10 Write a program that simulates a bank withdrawal. If the withdrawal amount is greater than the balance, raise a custom ValueError saying "Insufficient balance!".
 balance = 5000
 
 try:
@@ -107,3 +107,4 @@ try:
 
 except ValueError as amt:
     print(amt)
+
